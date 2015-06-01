bdc0271bd6b4cc1d138fc8e4d0395b1d3518f62d
Mario Quesada Moreno <mqm00006@red.ujaen.es>
2015-06-01 16:45:53 +0200

    configuracion de git

---
+++ Reja/app/src/main/java/com/quesada/utils/AdapterBusqueda.java
@@ -0,0 +1,84 @@
+package com.quesada.utils;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.quesada.reja.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by uni on 27/04/15.
+ */
+public class AdapterBusqueda extends BaseAdapter {
+
+    private ArrayList<Restaurante> entradas;
+    private int R_layout_IdView;
+    private Context contexto;
+
+    private static LayoutInflater inflater = null;
+
+    public AdapterBusqueda(Context contexto, ArrayList<Restaurante> entradas) {
+
+        this.contexto = contexto;
+        this.entradas = entradas;
+        inflater = (LayoutInflater) contexto
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return entradas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return entradas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        TextView nombre;
+        TextView direccion;
+        TextView rating;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_busqueda, parent, false);
+
+            nombre = (TextView) convertView.findViewById(R.id.nombre);
+            direccion = (TextView) convertView.findViewById(R.id.direccion);
+
+
+            convertView.setTag(R.id.nombre, nombre);
+            convertView.setTag(R.id.direccion, direccion);
+
+        } else {
+
+            nombre = (TextView) convertView.getTag(R.id.nombre);
+            direccion = (TextView) convertView.getTag(R.id.direccion);
+
+        }
+
+        nombre.setText(entradas.get(position).getNombre());
+        direccion.setText(entradas.get(position).getDireccion());
+
+        return convertView;
+    }
+
+    public void newAdapter(ArrayList<Restaurante> entradas) {
+        this.entradas = entradas;
+        inflater = (LayoutInflater) contexto
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+}
---
+++ Reja/app/src/main/java/com/quesada/utils/utils.java
@@ -0,0 +1,237 @@
+package com.quesada.utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by uni on 16/04/15.
+ */
+public class utils {
+
+
+    private static String servidor = "http://sinbad2.ujaen.es/reja/rejaapi/";
+    //private static String servidor = "http://192.168.56.1:1083/rejaapi/";
+
+    /**
+     *
+     * @param servicio
+     * @return
+     * @throws IOException
+     */
+
+
+    /**
+     *
+     * @param stream
+     * @return
+     * @throws IOException
+     * @throws UnsupportedEncodingException
+     */
+    public static JSONObject JSONParser(InputStream stream) throws IOException, UnsupportedEncodingException {
+        String json="";
+        JSONObject jObj=null;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    stream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            stream.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+            jObj=null;
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+
+        }
+
+        // return JSON String
+        return jObj;
+    }
+
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public static boolean isNetworkAvailable(Activity a) {
+        Context context = a.getApplicationContext();
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity == null) {
+            Toast.makeText(a.getBaseContext(), "No hay internet", Toast.LENGTH_LONG).show();
+        } else {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param a
+     */
+    public static void connectionMessage(Activity a)
+    {
+        new AlertDialog.Builder(a)
+                .setTitle("Error de Conexión")
+                .setMessage("Para recibir la recomendacion, es necesario tener conexion a la red.")
+                .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+
+    public static JSONObject recomendacion(String servicio) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+
+
+        try {
+            URL url = new URL(servidor+servicio);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+
+            conn.setRequestMethod("GET");
+            conn.addRequestProperty("Content-Type", "application/json");
+            conn.addRequestProperty("Charset","UTF-8");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("DEBUG_TAG", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            JSONObject contentAsString = JSONParser(is);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+        return null;
+    }
+
+    public static JSONObject postRequest(String servicio,List<NameValuePair> params)
+    {
+        InputStream is = null;
+
+        try {
+
+
+
+
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(servidor+servicio);
+
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+            JSONObject contentAsString = JSONParser(is);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+}
